2bac0f03192f52f04323d6f806a7bfba3a06f612
Joshuatanmy <140237486+Joshuatanmy@users.noreply.github.com>
2024-10-08 16:40:28 +1100

    reorganisation
    reorganisation

--- vega_lite_vis.js
+++
@@ -1,5 +0,0 @@
-var spec2 = "D:\Monash\FIT3179\Homework - Week9\WK9_BearAttacksMap.vg.json";
-
-vegaEmbed('#WK_9_MAP', spec2, { "actions": false }).then(function(result) {
-// Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
